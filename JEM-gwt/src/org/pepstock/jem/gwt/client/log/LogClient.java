acd02bc9617ea6c51f1f31b2fc7f4f35063c0192
Marco Cuccato <mcuccato@gmail.com>
2014-03-07 11:45:13 +0100

    Updated copyright years to 2014, with the format described here: https://www.gnu.org/licenses/gpl-howto.html

--- JEM-gwt/src/org/pepstock/jem/gwt/client/log/LogClient.java
+++ JEM-gwt/src/org/pepstock/jem/gwt/client/log/LogClient.java
@@ -1,6 +1,6 @@
 /**
     JEM, the BEE - Job Entry Manager, the Batch Execution Environment
-    Copyright (C) 2012, 2013 Marco "Cuc" Cuccato
+    Copyright (C) 2012-2014 Marco "Cuc" Cuccato
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
--- JEM/src/org/pepstock/jem/ant/validator/transformer/TransformerValidator.java
+++ JEM/src/org/pepstock/jem/ant/validator/transformer/TransformerValidator.java
@@ -1,6 +1,6 @@
 /**
     JEM, the BEE - Job Entry Manager, the Batch Execution Environment
-    Copyright (C) 2012, 2013   LucaC
+    Copyright (C) 2012-2014   LucaC
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
